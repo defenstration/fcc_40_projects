6fa86883ad9bc13727c979ef0c11fb67b4816764
Blackman <DBlackman@int.co.chenango.ny.us>
2024-10-15 08:31:01 -0400

    add ejs

---
+++ color_flipper/color_flipper.js
@@ -0,0 +1,21 @@
+
+
+
+const colors = [
+    'hsl(0, 100%, 50%)',    // Red
+    'hsl(30, 100%, 50%)',   // Orange
+    'hsl(60, 100%, 50%)',   // Yellow
+    'hsl(90, 100%, 50%)',   // Light Green
+    'hsl(120, 100%, 50%)',  // Green
+    'hsl(150, 100%, 50%)',  // Mint Green
+    'hsl(180, 100%, 50%)',  // Cyan
+    'hsl(210, 100%, 50%)',  // Sky Blue
+    'hsl(240, 100%, 50%)',  // Blue
+    'hsl(270, 100%, 50%)',  // Purple
+    'hsl(300, 100%, 50%)',  // Magenta
+    'hsl(330, 100%, 50%)',  // Pink
+    'hsl(0, 0%, 50%)',      // Gray
+    'hsl(180, 50%, 25%)',   // Dark Cyan
+    'hsl(60, 80%, 90%)'     // Light Yellow
+  ];
+  
