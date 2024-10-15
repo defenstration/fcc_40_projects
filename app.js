e5a83747566366b33fadc5924bd505d0afcf1701
Blackman <DBlackman@int.co.chenango.ny.us>
2024-10-15 15:11:41 -0400

    end of day

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,18 @@ const port = 3000;
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended : true}));
 
+//app.set("views", path.join(__dirname, "views"))
+
 app.get("/", (req, res) => {
     res.render('index.ejs')
+})
+
+app.get("/projects", (req, res) => {
+    res.render("projects/projects.ejs")
+})
+
+app.get("/color-flipper", (req, res) => {
+    res.render("projects/projects/color-flipper/color-flipper.ejs")
 })
 
 app.listen(port, () => {
---
+++ views/projects/projects/color-flipper/color-flipper.js
@@ -0,0 +1,25 @@
+const colorBtn = document.querySelector(".color-flip-btn")
+
+const colors = [
+    { "Red": 'hsl(0, 100%, 50%)' },
+    { "Orange": 'hsl(30, 100%, 50%)' },
+    { "Yellow": 'hsl(60, 100%, 50%)' },
+    { "Light Green": 'hsl(90, 100%, 50%)' },
+    { "Green": 'hsl(120, 100%, 50%)' },
+    { "Mint Green": 'hsl(150, 100%, 50%)' },
+    { "Cyan": 'hsl(180, 100%, 50%)' },
+    { "Sky Blue": 'hsl(210, 100%, 50%)' },
+    { "Blue": 'hsl(240, 100%, 50%)' },
+    { "Purple": 'hsl(270, 100%, 50%)' },
+    { "Magenta": 'hsl(300, 100%, 50%)' },
+    { "Pink": 'hsl(330, 100%, 50%)' },
+    { "Gray": 'hsl(0, 0%, 50%)' },
+    { "Dark Cyan": 'hsl(180, 50%, 25%)' },
+    { "Light Yellow": 'hsl(60, 80%, 90%)' }
+  ];
+  
+
+colorBtn.addEventListener("click", () => {
+    let number = Math.floor(Math.random() * 15)
+    console.log(number)
+})
